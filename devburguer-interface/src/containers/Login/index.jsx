5e32ddc6add39008dab8211e1891c216cf68a366
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-12-17 17:50:05 -0300

    10 - Validando o formulário de Login

--- devburguer-interface/src/containers/Login/index.jsx
+++ devburguer-interface/src/containers/Login/index.jsx
@@ -1,7 +1,29 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup'
+
+
 import { Container, LeftContainer, RightContainer, Title, Form, InputContainer, } from "./styles"
+
 import logo from "../../assets/Logo 1.svg"
 import { Button } from "../../components/Button"
 export default function Login() {
+    const schema = yup.object({
+        email: yup.string().email().required(),
+        password: yup.string().min(6).required(),
+    }).required()
+
+
+    const { register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
         <Container>
 
@@ -13,18 +35,19 @@ export default function Login() {
                 <Title>Olá, seja bem vindo ao <span>Dev
                     Burguer!</span>
                     <br />
-                    Acesse com seu <span> Login e senha.</span></Title>
-                <Form>
+                    Acesse com seu <span> Login e senha.</span>
+                </Title>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <InputContainer>
                         <label>Email</label>
-                        <input type="email" />
+                        <input type="email" {...register("email")} />
                     </InputContainer>
                     <InputContainer>
                         <label>Senha</label>
-                        <input type="password" />
+                        <input type="password" {...register("password")} />
                     </InputContainer>
                     {/* <Link>Esqueci minha senha</Link> */}
-                    <Button buttonType="blue" type="submit">Entrar</Button>
+                    <Button type="submit">Entrar</Button>
                 </Form>
                 <p>Não possui conta? <a href="#">Clique aqui!</a></p>
             </RightContainer>
