b821953f187d1cd81387c68a78d7c5dab6b95700
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-12-18 20:04:47 -0300

    feat: Add initial React project setup with Vite, Axios, and development dependencies.

--- devburguer-interface/src/services/api.js
+++ devburguer-interface/src/services/api.js
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios from "axios"
+
 
 export const api = axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "http://localhost:3001"  // impor.env.VITE_BAE_URL,
 })
