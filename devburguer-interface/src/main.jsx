19add173820f7cbe0ee0cd4780cb49d1e2650bea
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-12-19 23:25:54 -0300

    criando rota de usuario

--- devburguer-interface/src/main.jsx
+++ devburguer-interface/src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Login from './containers/Login'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
 import GlobalStyle from './styles/globalStyles'
 import { ToastContainer } from 'react-toastify'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <GlobalStyle />
 
-    <Login />
     <ToastContainer autoClose={3000} theme='dark' />
   </StrictMode>,
 )
