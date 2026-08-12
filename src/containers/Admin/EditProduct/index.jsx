f3db64686127433b5beed4e007bf0a96be619a90
Menephyl <ymenephyl@gmail.com>
2026-08-12 00:45:41 -0300

    feat: implement admin product edit container and styling

--- src/containers/Admin/EditProduct/index.jsx
+++ src/containers/Admin/EditProduct/index.jsx
@@ -6,7 +6,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { api } from '../../../services/api'
 import { toast } from 'react-toastify'
 
-import { Button } from '../../../components'
+
 import {
     Container,
     ContainerCheckbox,
@@ -33,10 +33,128 @@ const schema = yup.object({
 })
 
 export function EditProduct() {
+    const [fileName, setFileName] = useState(null)
+    const [categories, setCategories] = useState('')
 
+    const navigate = useNavigate()
 
+    const { state } = useLocation()
 
-    const schema = yup.object({
+    const {
+        state: { produto },
+    } = useLocation()
+
+    useEffect(() => {
+        async function loadCategories() {
+            const { data } = await api.get('/categories')
+            setCategories(data)
+
+        }
+        loadCategories()
+    }, [])
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema),
 
     })
+    const onSubmit = async (data) => {
+        const newFormData = new FormData()
+
+        newFormData.append('name', data.name);
+        newFormData.append('price', data.price);
+        newFormData.append('category_id', data.category.id)
+        newFormData.append('file', data.file[0])
+        newFormData.append('offer', data.offer)
+
+        await toast.promise(api.put(`/products/${produto.id}`, newFormData), {
+            pending: 'Editando produto...',
+            success: 'Produto editado com suceso!',
+            error: 'Erro ao editar o produto',
+        })
+        setTimeout(() => {
+            navigate('/admin/produtos')
+        }, 2000)
+
+
+    }
+    return (
+        <>
+            <Container>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <InputGroup>
+                        <Label>Nome</Label>
+                        <Input type="text"
+                            {...register('name')}
+                            defaultValue={produto.name}
+                        />
+                        <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+
+                    </InputGroup>
+
+
+                    <InputGroup>
+                        <Label>Preço</Label>
+                        <Input type="number"
+                            {...register('price')}
+                            defaultValue={produto.price / 100}
+                        />
+                        <ErrorMessage>{errors?.price?.message}</ErrorMessage>
+                    </InputGroup>
+
+                    <InputGroup>
+                        <LabelUpload>
+                            <Image />
+                            <input type="file" {...register('file')}
+                                accept='image/jpeg,image/jpg,image/png'
+                                onChange={(value) => {
+                                    setFileName(value?.target?.files[0]?.name)
+                                    register('file').onChange(value)
+                                }}
+                            />
+                            {fileName || 'Upload do Produto'}
+
+                        </LabelUpload>
+                        <ErrorMessage>{errors.file?.message}</ErrorMessage>
+                    </InputGroup>
+
+                    <InputGroup>
+                        <Label>Categoria</Label>
+                        <Controller
+                            name="category"
+                            control={control}
+                            defaultValue={produto.category}
+                            render={({ field }) => (
+                                <Select
+                                    {...field}
+                                    options={categories}
+                                    getOptionLabel={(categories) => categories.name}
+                                    getOptionValue={(categories) => categories.id}
+                                    placeholder="Selecione uma categoria"
+                                    menuPortalTarget={document.body}
+                                    defaultValue={produto.category}
+                                />
+
+                            )}
+                        />
+                        <ErrorMessage>{errors.category?.message}</ErrorMessage>
+                    </InputGroup>
+                    <InputGroup>
+                        <ContainerCheckbox>
+                            <input type="checkbox" {...register('offer')}
+                                defaultChecked={produto.offer}
+                            />
+                            <Label>Produto em Oferta ? </Label>
+                        </ContainerCheckbox>
+                    </InputGroup>
+                    <SubmitButton type="submit">Editar Produto </SubmitButton>
+                </Form>
+            </Container >
+        </>
+    )
+
 }
