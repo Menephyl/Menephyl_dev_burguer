e2163faa2d4c8eb0ced4194bfc371ef13f96093f
Menephyl <ymenephyl@gmail.com>
2026-08-15 00:08:16 -0300

    feat: implement admin product edit container and styling

---
+++ src/containers/Admin/NewProduct/style.js
@@ -0,0 +1,89 @@
+import ReactSelect from 'react-select';
+import styled from "styled-components"
+import { Button } from '../../../components';
+
+
+export const Container = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+
+`
+
+export const From = styled.form`
+border-radius:20px;
+background-color:${(props) => props.theme.black};
+padding:32px;
+width: 100%;
+max-width:380px;
+display:flex;
+flex-direction:column;
+gap:12px;
+
+`
+
+export const InputGroup = styled.div`
+display:flex;
+flex-direction:column;
+gap: 4px;
+
+
+`
+
+export const Label = styled.label`
+color:${(props) => props.theme.white};
+font-size:14px;
+`
+
+export const LabelUpload = styled.label`
+cursor: pointer; 
+border: 1px dashed ${(props) => props.theme.white};
+border-radius: 5px; 
+padding:10px;
+display:flex;
+color:${(props) => props.theme.white};
+margin:20px 0; 
+
+>svg{
+    width: 20px;
+    height:20px;
+    fill: ${(props) => props.theme.white};
+    margin-right:4px;
+
+}
+input{
+    display: none;
+}
+
+`
+
+export const Select = styled(ReactSelect)`
+
+
+
+`
+
+export const SubmitButton = styled(Button)`
+margin-top:20px;
+`
+
+export const ErrorMessage = styled.p`
+color:${(props) => props.theme.darkRed};
+font-size:14px;
+line-height: 80%;
+font-weight: 600;
+
+`
+export const ContainerCheckox = styled.div`
+display:flex;
+gap:20px;
+cursor:pointer;
+margin-top:10px;
+
+input{
+    cursor:pointer;
+}
+
+
+`
